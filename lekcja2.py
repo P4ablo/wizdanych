736f5a5d0053131e37de120265b8c0068dda410f
P4ablo <58783615+P4ablo@users.noreply.github.com>
2020-03-04 18:42:52 +0100

    Add files via upload

---
+++ lekcja2.py
@@ -0,0 +1,82 @@
+#ZAD1
+#napis = input()
+#x = napis.count(" ")
+#print(x)
+#-------------------------------------------------
+
+#ZAD2
+#import sys
+#print("Podaj 1 liczbe: ")
+#a = sys.stdin.readline()
+#a=int(a)
+#print("Podaj 2 liczbe: ")
+#b = sys.stdin.readline()
+#b=int(b)
+#wynik=a*b
+#print(wynik)
+#--------------------------------------------------
+
+#ZAD4
+#from math import * 
+#a = input("Podaj liczbe: ")
+#a=int(a)
+#print(fabs(a))
+#-------------------------------------------------
+
+#ZAD5
+#a = input("Podaj 1 liczbe: ")
+#a=int(a)
+#b = input("Podaj 2 liczbe: ")
+#b=int(b)
+#c = input("Podaj 3 liczbe: ")
+#c=int(c)
+#if 0<a<=10 and a>b or b>c:
+    #print("Liczba a jest w przedziale (0,10> i jednoczesnie a>b lub b>c")
+#else:
+    #print("Warunki nie spelnione!")
+#-----------------------------------------------
+
+#ZAD6
+#for x in range(1,26,1):
+    #if x%5==0:
+        #print(x)
+#----------------------------------------------        
+
+#ZAD7 ---------------------------->DO ZROBIENIA
+#tupla=input("Podaj liczby: ")
+#for x in range(1,len(tupla)):
+    #x=x*x
+    #print(x) 
+#--------------------------------------------
+
+#ZAD8 --------------------->DO ZROBIENIA
+#lista=[]
+#for x in range(len(lista)):
+    #lista.append
+#--------------------------------------------
+
+#ZAD 9 ---------------->DO ZROBIENIA
+#x=input()
+#x=str(x)
+#-------------------------------------------
+
+#ZAD10 --------------> PRZYPOMNIEC SB
+#j=int(input())
+#m=3
+#for i in range(0,j):
+    #for k in range(1,m):
+        #print(k*"#")
+    #m+=1
+#------------------------------------------
+
+#ZAD11
+#symbol = "o"
+
+#print(f"{symbol*1:^7}") 
+#print(f"{symbol*3:^7}")
+#print(f"{symbol*5:^7}")
+#print(f"{symbol*7:^7}")
+#print(f"{symbol*5:^7}")
+#print(f"{symbol*3:^7}")
+#print(f"{symbol*1:^7}")
+#-----------------------------------------------
