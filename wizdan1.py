2e3f2f91d99d8b3df9297131110a32832654c856
P4ablo <58783615+P4ablo@users.noreply.github.com>
2020-03-05 20:24:57 +0100

    Update wizdan1.py

--- wizdan1.py
+++ wizdan1.py
@@ -1,12 +1,12 @@
 #print("Hello world!")
 
-a = 5
+#a = 5
 #print(type(a))
 
-imie = "Ala"
-imie = """Ala
-ma kota
-Filemona"""
+#imie = "Ala"
+#imie = """Ala
+#ma kota
+#Filemona"""
 #print(imie)
 
 #def funkcje():
@@ -21,9 +21,9 @@ Filemona"""
 #liczba = int("5")
 #liczba = int("5.5")
 
-imie="ala"
+#imie="ala"
 #print(imie.capitalize())
-imie=imie.capitalize()
+#imie=imie.capitalize()
 #print(imie)
 
 #print(imie[0])
@@ -40,33 +40,33 @@ imie=imie.capitalize()
 #print(imie+"ma"+str(5)+"lat")
 #print("{} ma {} lat".format(imie,5))
 #print("{0} ma {1} lat".format(imie,5))
-wiek = 5
+#wiek = 5
 #print(f"{imie} ma {wiek} lat")
 #---------------------------------------------------------------------
 
 ##LISTY
 
-lista = []
-lista = [1, 3, 4, "Ala", 4.5, imie]
-lista = [1, 3, 4, "Ala", 4.5, imie, [1, 2]]
-lista[0]
+#lista = []
+#lista = [1, 3, 4, "Ala", 4.5, imie]
+#lista = [1, 3, 4, "Ala", 4.5, imie, [1, 2]]
+#lista[0]
 #print(lista[5][1]) ---->lista dwupoziomowa
 
-lista.append(3)
-lista[0] = 10
+#lista.append(3)
+#lista[0] = 10
 
-lista2 = lista + lista
+#lista2 = lista + lista
 #print(lista2)
 #--------------------------------------------------------------------
 
 #SLOWNIK
 
-slownik = {}
-slownik = {"imie": "Marek", 
-"wiek":35}
+#slownik = {}
+#slownik = {"imie": "Marek", 
+#"wiek":35}
 #print(slownik)
 
-slownik["imie"] #----------->wybieranie rzeczy ze slownika
+#slownik["imie"] #----------->wybieranie rzeczy ze slownika
 #print(slownik.items())
 #---------------------------------------------------------------------
 
@@ -74,5 +74,64 @@ slownik["imie"] #----------->wybieranie rzeczy ze slownika
 
 #from math import *
 #import math as m ------->better option
+
+#ZAD1
+#a=2
+#b="Hello"
+#c=2.0
+#print(a)
+#print(b)
+#print(c)
+#-----------------------------------------------------------------------
+
+#ZAD2
+#x=input("Podaj 1 liczbe: ")
+#y=input("Podaj 2 liczbe: ")
+#x=int(x)
+#y=int(y)
+#a=x+y
+#b=x-y
+#c=x*y
+#d=x/y
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+#-----------------------------------------------------------------------
+
+#ZAD4
+#from math import *
+#print(pow(e,10))
+#---------------------------------------------------------------------
+
+#ZAD5
+#x="PAWEL"
+#y="DABEK"
+#print(x.capitalize())
+#print(y.capitalize())
+#--------------------------------------------------------------------
+
+#ZAD6
+#tekst="la la la la la"
+#print(tekst.count("la"))
+#--------------------------------------------------------------------
+
+#ZAD7
+#x="Pablo"
+#print(x[1])
+#print(x[4])
+#--------------------------------------------------------------------
+
+#ZAD8
+#tekst="la,la,la,la,la"
+#print(tekst.split(","))
+#--------------------------------------------------------------------
+
+#ZAD10
+#lista=["Top Gun","Forest Gump","1917","Scarface"]
+#print("Ulubione filmy:\n",lista)
+#lista.sort()
+#print("Ulubione posortowane filmy:\n",lista)
+#--------------------------------------------------------------------
 
  
