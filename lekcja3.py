1580725cf7a01d80aaf527956479f0ec18420816
P4ablo <58783615+P4ablo@users.noreply.github.com>
2020-03-11 17:44:32 +0100

    Add files via upload

---
+++ lekcja3.py
@@ -0,0 +1,28 @@
+#import this-------------->jakies gowno
+
+#imie="Marianna"
+#lista=[]
+#for litera in imie:
+    #lista.append(litera.upper())
+#print(lista)
+
+#print([litera.upper() for litera in imie])
+
+#print([2**n for n in range(5)])
+
+#imie='Pablo'
+#(0, 'P')
+#{0:'P',...}
+#{para[0]:para[1] litera for para in enumerate(imie)}
+#{para[0]:para[1] litera for para in enumerate(imie)}
+
+#from timeit import timeit
+
+#def dodaj(liczba1, liczba2):
+    #return liczba1+liczba2
+#print(dodaj(2,3))
+
+def witaj(imie='Jan'):
+    print(f'Witaj {imie}!')
+witaj()
+witaj('PAAABLOOO')
